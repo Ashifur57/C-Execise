df9a3cfc001925910447902151cd572ea2975e5b
Ashifur Rahman <59836503+Ashifur57@users.noreply.github.com>
2022-02-21 01:48:28 +0600

    Add files via upload

---
+++ EntityFramework/ZooAPP/ZooApp.Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace ZooApp.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Food { get; set; }
+
+        public string Origin { get; set; }
+
+        public int Quantity { get; set; }
+
+
+    }
+}
