e33712b306de83249059a4b08f7bd93d13c78fe8
Ashifur Rahman <59836503+Ashifur57@users.noreply.github.com>
2022-02-21 01:33:26 +0600

    Add files via upload

---
+++ Basic/Accasing_Class/Accasing_Class/home/Home_Town.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accasing_Class.home
+{
+    public class Home_Town
+    {
+        public static string Home(String S1, String S2) 
+        {
+            return S1 +" "+ S2; 
+        }
+    }
+}
